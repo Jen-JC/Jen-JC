f374b35ddb76bb1aad0d16a1ad0ea60d17e1f944
Jen-JC <46200967+Jen-JC@users.noreply.github.com>
2018-12-27 13:46:35 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ var currentImg = document.querySelector('img');
 currentImg.onclick = function () {
     var imgSrc = currentImg.getAttribute('src'); //fetches the source of the image clicked
 
-    if (imgSrc === "sheep.png") { // check to see if sheep
+    if (imgSrc === "images/sheep.png") { // check to see if sheep
         currentImg.setAttribute('src', 'images/pig.png'); //change to pig
     } else {
         currentImg.setAttribute('src', 'images/sheep.png'); // or back to sheep
